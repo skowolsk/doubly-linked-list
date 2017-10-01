5c42940753dafa419a39e663bc66d01d57a36e11
skowolsk <31890900+skowolsk@users.noreply.github.com>
2017-10-01 15:04:09 +0200

    Update linked-list.js

--- src/linked-list.js
+++ src/linked-list.js
@@ -39,25 +39,24 @@ class LinkedList {
     }
 
     insertAt(index, data) {
-        let temp;
-        let newIndex = 0;
-        let list =new Node(data);
-
-        if(!temp) {
-            newIndex++;
-            temp = this.head;
-        }
-
-        while(newIndex <= index){
-            temp = temp.next;
-            temp.prev.next = list;
-            list.prev = temp.prev;
-            temp.prev = list;
-            list.next = temp;
-            newIndex++;
-        }
-
-        return this;
+        let list = new Node(data);
+            if (this.isEmpty()) {
+                this._head = list;
+                this._tail = list;
+            }
+            else if (index === this.length) {
+                this.append(data);
+            } else {
+                for (let i = 0; i < index-1; i++){
+                    this.head = this.head.next;
+                }
+                list.prev = this.head.prev;
+                list.next = this.head;
+                this.head.prev.next = list;
+                this.head.prev = list;
+            }
+            this.length++;
+            return this;
     }
 
     isEmpty() {
